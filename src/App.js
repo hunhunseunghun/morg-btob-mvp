b1b9b28d67b558665f3b1f4bbc58de43b42add85
seunghun <hunhunseunghun@gmail.com>
2023-04-06 13:54:40 +0900

    firebase learning

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
 import './App.css';
 import {useEffect} from "react"
+
+// Firebase
+import firebase from "firebase/compat/app";
 import {firestore} from "./Firebase.jsx"
 import { collection, getDocs} from "firebase/firestore"
 
 function App() {
 
   useEffect(()=>{
-   console.log(firestore)
+    const db = firebase.firestore()
+    db.collection("user").get().then((res)=>{
+      console.log("res", res)
+      res.forEach((doc)=>{
+        console.log("doc:",doc.data())
+      })
+    })
   },[])
 
 
-  const getData = async(db) =>{
-    const querySnapshot = await getDocs(collection(db, "user"));
-querySnapshot.forEach((doc) => {
-  console.log(`${doc.id} => ${doc.data()}`);
-});
-  }
+ 
 
  
   return (
     <div className="App">
-      <iframe src="https://en.wikipedia.org/wiki/Tutorial"></iframe>
+      <iframe width="100%" height="700px" src="https://lookerstudio.google.com/embed/reporting/8a143aea-111f-473a-bd41-26f9e6a29f5f/page/p_xbg82f1i4c"  frameborder="0" style={{border:0}} ></iframe>
     </div>
   );
 }
